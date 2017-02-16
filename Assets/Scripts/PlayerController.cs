25433a100b9dc7e7d0197dab5d9a54e18ea501c8
Coolteenz <hugo.sacchi@gmail.com>
2017-02-16 11:16:07 +0100

    Test level, Test Character.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    float maxSpeed = 10f;
+    [SerializeField]
+    bool facingRight = true;
+    [SerializeField]
+    bool grounded = false;
+    [SerializeField]
+    float groundRadius = 0.2f;
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    LayerMask whatIsGround;
+    [SerializeField]
+    float jumpForce = 350f;
+    //[SerializeField]
+    //AudioClip soundJump;
+    //AudioSource m_Sound;
+    //Animator anim;
+
+
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+       //m_Sound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        if (grounded && Input.GetButtonDown("Jump"))
+        {
+            //m_Sound.PlayOneShot(soundJump);
+            //anim.SetBool("Ground", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+
+    }
+
+
+
+    void FixedUpdate()
+    {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        //anim.SetBool("Ground", grounded);
+
+        //anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+
+
+        float move = Input.GetAxis("Horizontal");
+
+        //anim.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.y);
+
+
+        GetComponent<Rigidbody2D>().velocity = new Vector3(maxSpeed * move, GetComponent<Rigidbody2D>().velocity.y);
+
+
+        //anim.SetFloat("Speed", Mathf.Abs(move));
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+
+
+    }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+
+
+        
+}
